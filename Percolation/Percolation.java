3babc9614fe78551bd96d76a67292f9d9fbe66c0
oriBetelgeuse <arkhipkin.v98@gmail.com>
2020-10-28 20:59:59 +0300

    Finding the critical probability in the problem of site percolation

---
+++ Percolation/Percolation.java
@@ -0,0 +1,82 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+public class Percolation {
+
+    private int size;
+    private int[][] grid;
+    private WeightedQuickUnionUF connection;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        assert n > 0;
+
+        size = n;
+        grid = new int[n][n];
+        connection = new WeightedQuickUnionUF(n * n + 2);
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = 0;
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        assert row >= 1 && row <= size;
+        assert col >= 1 && col <= size;
+
+        grid[row - 1][col - 1] = 1;
+
+        if (row == 1)
+            connection.union(size * size, (row - 1) * size + col - 1);
+        if (row == size)
+            connection.union(size * size + 1, (row - 1) * size + col - 1);
+
+        if (col - 1 <= size && col - 1 >= 1 && isOpen(row, col - 1))
+            connection.union(size * (row - 1) + col - 1, size * (row - 1) + col - 2);
+        if (col + 1 <= size && col + 1 >= 1 && isOpen(row, col + 1))
+            connection.union(size * (row - 1) + col - 1, size * (row - 1) + col);
+        if (row - 1 <= size && row - 1 >= 1 && isOpen(row - 1, col))
+            connection.union(size * (row - 1) + col - 1, size * (row - 2) + col - 1);
+        if (row + 1 <= size && row + 1 >= 1 && isOpen(row + 1, col))
+            connection.union(size * (row - 1) + col - 1, size * row + col - 1);
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        assert row >= 1 && row <= size;
+        assert col >= 1 && col <= size;
+
+        return grid[row - 1][col - 1] == 1;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        assert row >= 1 && row <= size;
+        assert col >= 1 && col <= size;
+
+        return connection.find((row - 1) * size + (col - 1)) == connection.find(size * size);
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        int sum = 0;
+
+        for (int[] row : grid)
+            for (int value : row)
+                sum += value;
+
+        return sum;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return connection.find(size * size) == connection.find(size * size + 1);
+    }
+}
---
+++ Percolation/PercolationStats.java
@@ -0,0 +1,75 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+
+    private double[] critProb;
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        assert n > 0;
+        assert trials > 0;
+
+        Percolation grid;
+        critProb = new double[trials];
+        int[] indexes = new int[n * n];
+
+        for (int i = 0; i < trials; i++) {
+            grid = new Percolation(n);
+            int iterator = n * n;
+            for (int j = 0; j < n * n; j++)
+                indexes[j] = j;
+
+            while (!grid.percolates()) {
+                int randIndex = StdRandom.uniform(iterator);
+                int index = indexes[randIndex];
+
+                indexes[randIndex] = indexes[iterator - 1];
+                iterator--;
+
+                grid.open(index / n + 1, index % n + 1);
+            }
+
+            critProb[i] = ((double) grid.numberOfOpenSites()) / (n * n);
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(critProb);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(critProb);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(critProb.length);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(critProb.length);
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        PercolationStats stat = new PercolationStats(Integer.parseInt(args[0]),
+                                                     Integer.parseInt(args[1]));
+
+        System.out.println(String.format("mean = %f", stat.mean()));
+        System.out.println(String.format("stddev = %f", stat.stddev()));
+        System.out.println(
+                String.format("95%% confidence interval = [%f, %f]", stat.confidenceLo(),
+                              stat.confidenceHi())
+        );
+    }
+}
